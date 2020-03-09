13bdbad83dc5b53d2493f40e0cdd64b1613c3931
aetoksoz <aetoksoz@gmail.com>
2020-03-09 13:04:46 +0300

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Alperen Toksöz on 9.03.2020.
+//  Copyright © 2020 Alperen Toksöz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
